5f4b6b60e55f8bfd48f0ca4ddbff115951720ed2
Jhon S. García Barrera <jhons.garciab@uqvirtual.edu.co>
2026-02-22 18:57:13 -0500

    Update load bronze layer

--- proyecto 1/scripts/sql/01_load_bronze_data.sql
+++ proyecto 1/scripts/sql/01_load_bronze_data.sql
@@ -15,179 +15,51 @@ TRUNCATE TABLE bronze.staging_nasa_daily;
 -- =========================================================
 -- IDEAM temperature (2015-2025)
 -- =========================================================
-COPY bronze.staging_ideam_temperature
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2015-01-01_2015-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_temperature
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2016-01-01_2016-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_temperature
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2017-01-01_2017-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_temperature
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2018-01-01_2018-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_temperature
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2019-01-01_2019-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_temperature
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2020-01-01_2020-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_temperature
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2021-01-01_2021-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_temperature
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2022-01-01_2022-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_temperature
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2023-01-01_2023-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_temperature
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2024-01-01_2024-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_temperature
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2025-01-01_2025-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_temperature (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2015-01-01_2015-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_temperature (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2016-01-01_2016-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_temperature (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2017-01-01_2017-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_temperature (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2018-01-01_2018-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_temperature (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2019-01-01_2019-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_temperature (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2020-01-01_2020-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_temperature (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2021-01-01_2021-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_temperature (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2022-01-01_2022-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_temperature (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2023-01-01_2023-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_temperature (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2024-01-01_2024-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_temperature (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/temperature/ideam_temperature_risaralda_pereira_2025-01-01_2025-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
 
 -- =========================================================
 -- IDEAM precipitation (2016-2025)
 -- =========================================================
-COPY bronze.staging_ideam_precipitation
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2016-01-01_2016-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_precipitation
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2017-01-01_2017-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_precipitation
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2018-01-01_2018-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_precipitation
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2019-01-01_2019-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_precipitation
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2020-01-01_2020-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_precipitation
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2021-01-01_2021-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_precipitation
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2022-01-01_2022-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_precipitation
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2023-01-01_2023-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_precipitation
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2024-01-01_2024-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_precipitation
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2025-01-01_2025-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_precipitation (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2016-01-01_2016-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_precipitation (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2017-01-01_2017-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_precipitation (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2018-01-01_2018-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_precipitation (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2019-01-01_2019-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_precipitation (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2020-01-01_2020-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_precipitation (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2021-01-01_2021-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_precipitation (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2022-01-01_2022-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_precipitation (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2023-01-01_2023-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_precipitation (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2024-01-01_2024-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_precipitation (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/precipitation/ideam_precipitation_risaralda_pereira_2025-01-01_2025-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
 
 -- =========================================================
 -- IDEAM humidity (2015-2025)
 -- =========================================================
-COPY bronze.staging_ideam_humidity
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2015-01-01_2015-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_humidity
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2016-01-01_2016-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_humidity
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2017-01-01_2017-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_humidity
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2018-01-01_2018-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_humidity
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2019-01-01_2019-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_humidity
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2020-01-01_2020-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_humidity
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2021-01-01_2021-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_humidity
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2022-01-01_2022-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_humidity
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2023-01-01_2023-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_humidity
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2024-01-01_2024-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
-
-COPY bronze.staging_ideam_humidity
-(codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida)
-FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2025-01-01_2025-12-31_daily.csv'
-WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_humidity (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2015-01-01_2015-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_humidity (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2016-01-01_2016-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_humidity (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2017-01-01_2017-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_humidity (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2018-01-01_2018-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_humidity (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2019-01-01_2019-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_humidity (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2020-01-01_2020-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_humidity (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2021-01-01_2021-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_humidity (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2022-01-01_2022-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_humidity (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2023-01-01_2023-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_humidity (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2024-01-01_2024-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_ideam_humidity (codigoestacion,nombreestacion,departamento,municipio,fecha,valor_diario,unidadmedida) FROM './data/raw/ideam/humidity/ideam_humidity_risaralda_pereira_2025-01-01_2025-12-31_daily.csv' WITH (FORMAT csv, HEADER true);
 
 -- =========================================================
 -- NASA POWER daily (single file)
 -- =========================================================
-COPY bronze.staging_nasa_daily
-(year, mo, dy, t2m, prectotcorr, rh2m)
-FROM './data/raw/nasa/nasa_power/nasa_power_pereira_20150101_20251231_daily.csv'
-WITH (FORMAT csv, HEADER true);
+\copy bronze.staging_nasa_daily (year, mo, dy, t2m, prectotcorr, rh2m) FROM './data/raw/nasa/nasa_power/nasa_power_pereira_20150101_20251231_daily.csv' WITH (FORMAT csv, HEADER true);
 
 -- =========================================================
 -- Quick sanity checks
